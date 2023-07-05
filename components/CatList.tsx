b6b192cc3c90b780d8533eee47091c904206934a
mochi <mochimochidaifucu@icloud.com>
2023-07-05 10:00:01 +0900

    [update] カテゴリー取得処理追記

--- components/CatList.tsx
+++ components/CatList.tsx
@@ -1,7 +1,10 @@
+import { CategoriesResponseDto } from "@/types/CategoriesResponseDto"
+
 type Props = {
     mx: string
+    cat: CategoriesResponseDto
 }
-export default function CatList({ mx }: Props) {
+export default function CatList({ mx, cat }: Props) {
     return (
         <div
             className={`w-[27%] h-[50%] flex items-center justify-center shrink-0 bg-[#f1f1f1] rounded-xl mx-[2%] ${mx}`}
@@ -9,10 +12,13 @@ export default function CatList({ mx }: Props) {
             <div className={"w-[80%] flex items-center"}>
                 <div
                     className={
-                        "w-[7vw] h-[7vw] max-w-[40px] max-h-[40px] bg-[url(/images/demo.jpg)] bg-center bg-cover bg-no-repeat rounded mr-[5%]"
+                        "w-[7vw] h-[7vw] max-w-[40px] max-h-[40px] bg-center bg-cover bg-no-repeat rounded mr-[5%]"
                     }
+                    style={{
+                        backgroundImage: `url(${cat.category_image_path})`,
+                    }}
                 ></div>
-                <p className={"text-[13px] leading-4"}>桜うんぬん</p>
+                <p className={"text-[13px] leading-4"}>{cat.category_name}</p>
             </div>
         </div>
     )
