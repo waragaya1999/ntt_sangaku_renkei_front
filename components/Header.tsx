307547c454d520811a66c2730668fb18b106fd4b
MeitoKuriyama <meito.kuri@gmail.com>
2023-07-17 02:32:06 +0900

    [add] locationのとこやった
    レイアウトは後回しにしやす

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import { signIn, signOut, useSession } from "next-auth/react"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import useAuth from "./hooks/useAuth"
 type Props = {
     location: string
@@ -19,19 +19,67 @@ export default function Header({ location }: Props) {
             signOut()
         }
     }
-
+    const [dropdownOpen, setDropdownOpen] = useState(false)
     const imageUrl = "/images/user.svg"
     return (
         <header className={"w-full max-w-[600px] h-12 fixed bg-white z-[999]"}>
             <div className={"w-[90%] h-[100%] flex justify-between m-auto"}>
-                <div className={"flex items-center"}>
-                    <Image
-                        src={"/images/user.svg"}
-                        alt="logo"
-                        width={40}
-                        height={40}
-                    />
-                    <h2 className={"whitespace-nowrap ml-[25%]"}>{location}</h2>
+                <div className={"flex items-center position-relative"}>
+                    <div className={"flex items-center"}>
+                        <Image
+                            src={"/images/user.svg"}
+                            alt="logo"
+                            width={40}
+                            height={40}
+                        />
+                        <div className="relative ml-[25%]">
+                            <h2
+                                className={"whitespace-nowrap cursor-pointer"}
+                                onClick={() => setDropdownOpen(!dropdownOpen)}
+                            >
+                                {location}
+                            </h2>
+                            {dropdownOpen && (
+                                <div className="absolute left-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                                    <div
+                                        className="py-1"
+                                        role="menu"
+                                        aria-orientation="vertical"
+                                        aria-labelledby="options-menu"
+                                    >
+                                        <a
+                                            href="/home"
+                                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                            role="menuitem"
+                                        >
+                                            ホーム
+                                        </a>
+                                        <a
+                                            href="/mypage"
+                                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                            role="menuitem"
+                                        >
+                                            マイページ
+                                        </a>
+                                        <a
+                                            href="/list"
+                                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                            role="menuitem"
+                                        >
+                                            リスト
+                                        </a>
+                                        <a
+                                            href="/postArticle"
+                                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                            role="menuitem"
+                                        >
+                                            投稿
+                                        </a>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
                 </div>
                 <div className={"flex items-center"}>
                     {session ? (
